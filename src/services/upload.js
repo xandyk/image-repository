bef57b077b636ce8c8a5f28eeb7fcbd5c8b4cfb1
Sandy K <skuan8@my.bcit.ca>
2021-01-10 18:48:12 -0800

    image upload successful

--- src/services/upload.js
+++ src/services/upload.js
@@ -1,30 +1,27 @@
-const path     = require('path')
-const aws      = require('aws-sdk')
-const uuid     = require('uuid').v4
-const multer   = require('multer')
-const multerS3 = require('multer-s3')
-
-const s3 = new aws.S3({apiVersion: '2006-03-01'})
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
 
 aws.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.REGION
-})
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.REGION,
+});
+
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
 const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket:'images.repository',
-        acl: 'public-read',
-        metadata: (req, file, cb) => {
-            cb(null, {fieldName: file.fieldname})
-        },
-        key: (req, file, cb) => {
-            const ext = path.extname(file.originalname)
-            cb(null, `${uuid()}${ext}`)
-        }
-    })
-})
+  storage: multerS3({
+    s3: s3,
+    bucket: 'images.repository',
+    acl: 'public-read',
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  }),
+});
 
-module.exports = upload
+module.exports = upload;
