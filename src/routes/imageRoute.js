f369b477c213df47acf8653716b312d406398362
Sandy K <skuan8@my.bcit.ca>
2021-01-11 23:09:11 -0800

    add view images route

--- src/routes/imageRoute.js
+++ src/routes/imageRoute.js
@@ -1,15 +1,15 @@
-const { json } = require('express');
 const express = require('express');
 const router = new express.Router();
 
 const upload = require('../services/upload');
+const viewImages = require('../services/view');
 
 const singleUpload = upload.single('photo');
 
 router.post(
   '/image-upload',
-  (req, res) => {
-    singleUpload(req, res, err => {
+  async (req, res) => {
+    await singleUpload(req, res, err => {
       res.send('Image uploaded!');
     });
     // return res.json({ status: 'OK', uploaded: req.file.length });
@@ -18,5 +18,12 @@ router.post(
     res.status(400).send('Please upload an image!');
   }
 );
+
+router.get('/image-view/', async (req, res) => {
+  await viewImages(req, res => {
+    res.send('Here are your images');
+  });
+  res.status(404).send();
+});
 
 module.exports = router;
