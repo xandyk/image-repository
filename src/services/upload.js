78999cc33725328d51dba215c5d1cb78bf688598
Sandy K <skuan8@my.bcit.ca>
2021-01-13 09:48:14 -0800

    remove config info from the file

--- src/services/upload.js
+++ src/services/upload.js
@@ -1,14 +1,7 @@
 const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-
-aws.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.REGION,
-});
-
-const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+const s3 = new aws.S3();
 
 const upload = multer({
   storage: multerS3({
