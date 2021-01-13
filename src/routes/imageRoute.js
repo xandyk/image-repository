70cc46dd0b31fa8d9fad226f854b7fd4f575637d
Sandy K <skuan8@my.bcit.ca>
2021-01-13 09:29:54 -0800

    have issues with GET and DELETE endpoints

--- src/routes/imageRoute.js
+++ src/routes/imageRoute.js
@@ -2,28 +2,40 @@ const express = require('express');
 const router = new express.Router();
 
 const upload = require('../services/upload');
-const viewImages = require('../services/view');
+const getImages = require('../services/display');
 
-const singleUpload = upload.single('photo');
+// POST an image
+router.post('/image-upload', async (req, res) => {
+  const singleUpload = upload.single('photo');
 
-router.post(
-  '/image-upload',
-  async (req, res) => {
-    await singleUpload(req, res, err => {
-      res.send('Image uploaded!');
-    });
-    // return res.json({ status: 'OK', uploaded: req.file.length });
-  },
-  (error, req, res, next) => {
-    res.status(400).send('Please upload an image!');
-  }
-);
-
-router.get('/image-view/', async (req, res) => {
-  await viewImages(req, res => {
-    res.send('Here are your images');
+  await singleUpload(req, res, err => {
+    if (err) {
+      res.send(err);
+    }
+    res.json({ message: 'Image is uploaded' });
   });
-  res.status(404).send();
+});
+
+// GET images
+router.get('/image-view', async (req, res) => {
+  await getImages(req, res, err => {
+    if (err) {
+      res.send(err);
+    }
+    res.json({ message: 'ok' });
+  });
+});
+
+// DELETE an image
+router.delete('/image-delete', (req, res) => {
+  const params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key: 'beach.jpg',
+  };
+  s3.deleteObject(params, (err, data) => {
+    if (err) console.log(err, err.stack);
+    else console.log('Successfully deleted');
+  });
 });
 
 module.exports = router;
