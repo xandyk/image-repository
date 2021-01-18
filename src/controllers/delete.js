0bea532a169a4c8b06d02b7833336a6ef8f69574
Sandy K <skuan8@my.bcit.ca>
2021-01-17 18:04:06 -0800

    add comment

--- src/controllers/delete.js
+++ src/controllers/delete.js
@@ -1,14 +1,22 @@
 const aws = require('aws-sdk');
 const s3 = new aws.S3();
 
-const deleteImage = async () => {
+const deleteImage = async (filename) => {
+
+// Delete an image from S3
+try {
   await s3
-    .deleteObject({
-      Bucket: process.env.BUCKET_NAME,
-      Key: 'beach.jpg',
-    })
-    .promise();
-  return { message: 'OK' };
+  .deleteObject({
+    Bucket: process.env.BUCKET_NAME,
+    Key: filename,
+  })
+  .promise();
+return { message: 'OK'};
+} catch (error) {
+  res.send(error)
+}
+
+ 
 };
 
 module.exports = deleteImage;
