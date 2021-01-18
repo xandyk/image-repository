ccca44a8678aa6fa501355a1303aa36d8b7d948f
Sandy K <skuan8@my.bcit.ca>
2021-01-17 18:00:58 -0800

    add get all images endpoint

--- src/routes/imageRoute.js
+++ src/routes/imageRoute.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = new express.Router();
 
 const upload = require('../controllers/upload');
-const download = require('../controllers/download');
 const getImages = require('../controllers/display');
 const deleteImage = require('../controllers/delete');
 const imageModel = require('../model/imageModel');
@@ -10,45 +9,55 @@ const searchImages = require('../controllers/search');
 
 const singleUpload = upload.single('photo');
 
-// POST an image
-router.post('/image-upload', singleUpload, async (req, res, next) => {
+// POST: upload an image with caption
+router.post('/image', singleUpload, async (req, res, next) => {
   try {
     const { caption } = req.body;
-    console.log(req.file);
-    console.log(caption);
     await imageModel.create({ name: req.file.location, caption });
-    res.json({ status: 'OK' });
+    res.status(201).json({ status: 'OK' });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// GET an image by its filename if its filename exists, if not, list all the images
+router.get('/image/:filename', async (req, res, next) => {
+
+  try {
+    // try to get an image by its filename if exists
+    if(req.params.filename !== null) {
+      
+      res.json({
+        filename: `https://s3-us-west-2.amazonaws.com/images.library/${req.params.filename}`
+      })
+      // send request 
+    } else {
+      const images = await getImages();
+      res.json(images);
+    }
   } catch (err) {
     next(err);
   }
 });
 
 // GET all images
-router.get('/image-view', async (req, res, next) => {
+router.get('/image/', async (req, res, next) => {
+
   try {
-    const images = await getImages();
-    res.json(images);
+      // send me a list of all images
+      const images = await getImages();
+      res.json(images);
+    
   } catch (err) {
     next(err);
   }
 });
 
-// GET: Download an image
-router.get('/image-download/:filename', async (req, res, next) => {
-  try {
-    const downloadFile = await download();
-    res.json(downloadFile);
-  } catch (err) {
-    next(err);
-  }
-});
-
-// Search for images
+// Search for an image
 router.get('/image-search/:keyword', async (req, res, next) => {
   try {
-    const {keyword} = req.params;
-    const search = await searchImages(keyword)
-    console.log(search, keyword)
+    const { keyword } = req.params;
+    const search = await searchImages.searchByCaption(keyword)
     res.json(search)
   } catch (err) {
     next(err)
@@ -56,9 +65,18 @@ router.get('/image-search/:keyword', async (req, res, next) => {
 } )
 
 // DELETE an image
-router.delete('/image-delete', async (req, res, next) => {
+router.delete('/image/:filename', async (req, res, next) => {
   try {
-    const remove = await deleteImage();
+    if(req.params.filename == null) {
+      return res.json({'Error': 'Filename is missing in the request' })
+    } else if (
+      //  check file exists using filename
+      !searchImages.searchByName(req.params.filename)
+    ) {
+      return res.json({'Error': 'File is missing in the database'})
+    }
+    // check if the filename is existing in the DB
+    const remove = await deleteImage(req.params.filename);
     res.json(remove);
   } catch (err) {
     next(err);
